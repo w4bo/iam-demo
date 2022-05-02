74d800e572e2e7c013261e0f66ac4da4a8dfac26
w4bo <m.francia@unibo.it>
2022-05-02 16:05:07 +0200

    fix build

--- intentional/init.sh
+++ intentional/init.sh
@@ -1,5 +1,10 @@
+echo "Replacing .env.example with .env"
 cp .env.example .env
+echo "Replacing start.sh with start.example.sh"
+cp start.example.sh start.sh
+echo "Replacing src/main/resources/config.example.yml src/main/resources/config.yml"
 cp src/main/resources/config.example.yml src/main/resources/config.yml
 P=$(pwd)
+echo $P
 sed -i "s+\!HOME\!+${P}+g" src/main/resources/config.yml
 sed -i "s+\!HOME\!+${P}+g" start.sh
--- intentional/start.example.sh
+++ intentional/start.example.sh
@@ -11,7 +11,7 @@ docker-compose up --build -d
 
 ./wait-for-it.sh ${ORACLE_IP}:${ORACLE_PORT} --strict --timeout=0 -- echo "ORACLE is up"
 
-export LD_LIBRARY_PATH=!HOME!/libs/instantclient_21_1
+export LD_LIBRARY_PATH=/home/mfrancia/iam-demo/intentional/libs/instantclient_21_1
 
 until [ -f resources/.ready ]
 do
