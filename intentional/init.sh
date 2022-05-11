8d41ec6ed558b10554454ec88fc9808beda2e3ff
w4bo <m.francia@unibo.it>
2022-05-11 18:14:09 +0200

    fix init

--- intentional/init.sh
+++ intentional/init.sh
@@ -4,10 +4,8 @@ echo "Replacing start.sh with start.example.sh"
 cp start.example.sh start.sh
 echo "Replacing src/main/resources/config.example.yml src/main/resources/config.yml"
 cp src/main/resources/config.example.yml src/main/resources/config.yml
-echo "Replacing ../intentional-web/js/config.example.js ../intentional-web/js/config.js"
-cp ../intentional-web/js/config.example.js ../intentional-web/js/config.js
-echo "Replacing ../intentional-assess/js/config.example.js ../intentional-assess/js/config.js"
-cp ../intentional-assess/js/config.example.js ../intentional-assess/js/config.js
+echo "Replacing ../web/js/config.example.js ../web/js/config.js"
+cp ../web/js/config.example.js ../web/js/config.js
 
 P=$(pwd)
 echo $P
