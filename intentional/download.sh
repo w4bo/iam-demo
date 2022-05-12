b2031883f0abff4de7bc6616dc79016d6b3b129e
Matteo Francia <m.francia@unibo.it>
2022-05-12 10:10:38 +0200

    Update download.sh

--- intentional/download.sh
+++ intentional/download.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 set -e
+set -xo
+
 cd resources
-curl -k -o foodmart-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql.sql
-curl -k -o foodmart-mysql-schema.sql https://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql-schema.sql
-curl -k -o COVID_WEEKLY.DMP https://big.csr.unibo.it/projects/nosql-datasets/COVID_WEEKLY.DMP
-curl -k -o FOODMART.DMP https://big.csr.unibo.it/projects/nosql-datasets/FOODMART.DMP
-curl -k -o FRENCHELECTRICITY.DMP https://big.csr.unibo.it/projects/nosql-datasets/FRENCHELECTRICITY.DMP
-curl -k -o FRENCHELECTRICITYEXT.DMP https://big.csr.unibo.it/projects/nosql-datasets/FRENCHELECTRICITYEXT.DMP
-curl -k -o SSB_FLIGHT.DMP https://big.csr.unibo.it/projects/nosql-datasets/SSB_FLIGHT.DMP
+if [! -f "foodmart-mysql.sql"] then 
+    curl -k -o foodmart-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql.sql
+fi
+if [! -f "foodmart-mysql-schema.sql"] then 
+    curl -k -o foodmart-mysql-schema.sql https://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql-schema.sql
+fi
+if [! -f "COVID_WEEKLY.DMP"] then 
+    curl -k -o COVID_WEEKLY.DMP https://big.csr.unibo.it/projects/nosql-datasets/COVID_WEEKLY.DMP
+fi
+if [! -f "FOODMART.DMP"] then 
+    curl -k -o FOODMART.DMP https://big.csr.unibo.it/projects/nosql-datasets/FOODMART.DMP
+fi
+if [! -f "FRENCHELECTRICITY.DMP"] then 
+    curl -k -o FRENCHELECTRICITY.DMP https://big.csr.unibo.it/projects/nosql-datasets/FRENCHELECTRICITY.DMP
+fi
+if [! -f "FRENCHELECTRICITYEXT.DMP"] then 
+    curl -k -o FRENCHELECTRICITYEXT.DMP https://big.csr.unibo.it/projects/nosql-datasets/FRENCHELECTRICITYEXT.DMP
+fi
+if [! -f "SSB_FLIGHT.DMP"] then 
+    curl -k -o SSB_FLIGHT.DMP https://big.csr.unibo.it/projects/nosql-datasets/SSB_FLIGHT.DMP
+fi
+
 ls -las
 cd -
+
 cd libs
-curl -k -o instantclient-basic-linux.x64-21.1.0.0.0.zip https://big.csr.unibo.it/projects/nosql-datasets/instantclient-basic-linux.x64-21.1.0.0.0.zip
-unzip instantclient-basic-linux.x64-21.1.0.0.0.zip
-curl -k -o instantclient-basic-windows.x64-21.3.0.0.0.zip https://big.csr.unibo.it/projects/nosql-datasets/instantclient-basic-windows.x64-21.3.0.0.0.zip
-unzip instantclient-basic-windows.x64-21.3.0.0.0.zip
+if [! -f "instantclient-basic-linux.x64-21.1.0.0.0.zip"] then 
+    curl -k -o instantclient-basic-linux.x64-21.1.0.0.0.zip https://big.csr.unibo.it/projects/nosql-datasets/instantclient-basic-linux.x64-21.1.0.0.0.zip
+    unzip instantclient-basic-linux.x64-21.1.0.0.0.zip
+    chmod -R 777 instantclient_21_1
+fi
+if [! -f "instantclient-basic-windows.x64-21.3.0.0.0.zip"] then 
+    curl -k -o instantclient-basic-windows.x64-21.3.0.0.0.zip https://big.csr.unibo.it/projects/nosql-datasets/instantclient-basic-windows.x64-21.3.0.0.0.zip
+    unzip instantclient-basic-windows.x64-21.3.0.0.0.zip
+    chmod -R 777 instantclient_21_3
+fi
 curl -k -o db-migration-0.1.0.jar https://big.csr.unibo.it/projects/nosql-datasets/db-migration-0.1.0.jar
+
 ls -las
 cd -
