95cbe2be093ca7be186cb21379fafe048cffed41
w4bo <w4bo@users.noreply.github.com>
2022-06-14 13:15:45 +0000

    Deploying to gh-pages from @ w4bo/iam-demo@e96e079542b9f72ab814bef03257ebfe17e83dc6 🚀

--- js/chart-bubble.js
+++ js/chart-bubble.js
@@ -5,7 +5,7 @@ function drawBubble(id, prop, data, c1, c2, c3, c4, mode) {
     let rawData = data["raw"]
     let minx = 0, maxx = 0, miny = 0, maxy = 0, minr = Number.MAX_SAFE_INTEGER, maxr = Number.MIN_SAFE_INTEGER;
     let domain = new Set();
-    let distinctc2 = new Set();
+    let distinct = new Set();
     rawData.forEach(function(d) {
         if (mode > 1) {
             minx = Math.min(minx, d[c1]);
@@ -16,9 +16,9 @@ function drawBubble(id, prop, data, c1, c2, c3, c4, mode) {
         minr = Math.min(minr, d[c3]);
         maxr = Math.max(maxr, d[c3]);
         domain.add(d[c4]);
-        distinctc2.add(d[c2]);
+        distinct.add(d[c2]);
     });
-    let count = distinctc2.size
+    let count = distinct.size
     domain = Array.from(domain);
     height = Math.max(400 - prop["margin"]["top"] - prop["margin"]["bottom"], count * 10);
     var div = d3.select("#" + id).append("div");
@@ -33,13 +33,8 @@ function drawBubble(id, prop, data, c1, c2, c3, c4, mode) {
     var y;
     // Add X axis
     if (mode > 1) {
-        x = d3.scaleLinear()
-            .domain([minx, maxx])
-            .range([0, width]);
-
-        y = d3.scaleLinear()
-            .domain([miny, maxy])
-            .range([height, 0]);
+        x = d3.scaleLinear().domain([minx, maxx]).range([0, width]);
+        y = d3.scaleLinear().domain([miny, maxy]).range([height, 0]);
     } else {
         x = d3
             .scalePoint()
@@ -61,22 +56,17 @@ function drawBubble(id, prop, data, c1, c2, c3, c4, mode) {
     }
 
     // Add a scale for bubble size
-    var z = d3
-        .scaleLinear()
-        .range([2.5, 10])
-        .domain([minr, maxr]);
-
-    var tooltip = createTooltip(div);
-    svg.append('g')
+    const z = d3.scaleLinear().range([2.5, 10]).domain([minr, maxr]);
+    const tooltip = createTooltip(div);
+    svg
         .selectAll("dot")
         .data(rawData)
         .enter()
         .append("circle")
-            .attr("transform", "translate(" + x(0) + "," + y(0) + ")")
             .attr("cx", function (d) { return x(d[c1]); } )
             .attr("cy", function (d) { return y(d[c2]); } )
             .attr("datapoint", "negcolored")
-            .attr("r", function (d) {  if (mode == 2 && c3 == "undefined") return 4; else return z(d[c3]); })
+            .attr("r", function (d) {  if (mode > 1 && typeof c3 == "undefined") return 4; else return z(d[c3]); })
             .on("mouseover",  function f (d) { showTooltip(data, d, tooltip) })
             .on("mousemove",  function f (d) { moveTooltip(d, tooltip) })
             .on("mouseleave", function f (d) { hideTooltip(d, tooltip) });
