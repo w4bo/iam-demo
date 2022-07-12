f24cb29e3edc32dead10fc61e8bb126a2a836cef
w4bo <m.francia@unibo.it>
2022-07-12 14:26:38 +0000

    Deploying to gh-pages from @ w4bo/iam-demo@eed3ebda2d546f4fc634cd6b6c6a4302356c44ab 🚀

--- js/chart-bubble.js
+++ js/chart-bubble.js
@@ -1,36 +1,39 @@
 function drawBubble(id, prop, data, c1, c2, c3, c4, mode) {
-
     // set the dimensions and margins of the graph
     let margin = prop.margin, width = prop.width, height = prop.height;
-    let rawData = data["raw"]
+    const rawData = data["raw"]
     let minx = 0, maxx = 0, miny = 0, maxy = 0, minr = Number.MAX_SAFE_INTEGER, maxr = Number.MIN_SAFE_INTEGER;
     let domain = new Set();
-    let distinct = new Set();
+    const distinctY = new Set();
+    const distinctX = new Set();
     rawData.forEach(function(d) {
-        if (mode > 1) {
+        if (mode > 1) { // both are numerical axis
             minx = Math.min(minx, d[c1]);
             maxx = Math.max(maxx, d[c1]);
             miny = Math.min(miny, d[c2]);
             maxy = Math.max(maxy, d[c2]);
+        } else { // both are categorical axis
+            distinctY.add(d[c2]);
+            distinctX.add(d[c1]);
         }
         minr = Math.min(minr, d[c3]);
         maxr = Math.max(maxr, d[c3]);
         domain.add(d[c4]);
-        distinct.add(d[c2]);
     });
-    let count = distinct.size
     domain = Array.from(domain);
-    height = Math.max(400 - prop["margin"]["top"] - prop["margin"]["bottom"], count * 10);
-    var div = d3.select("#" + id).append("div");
-    
-    var svg = div
+    if (mode == 1) {
+        height = Math.max(height, distinctY.size * 10);
+        width = Math.max(width, distinctX.size * 10);
+    }
+    const div = d3.select("#" + id);
+    const svg = div
                 .append("svg")
                     .attr("width", width + margin.left + margin.right)
                     .attr("height", height + margin.top + margin.bottom)
                 .append("g")
                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    var x;
-    var y;
+    let x;
+    let y;
     // Add X axis
     if (mode > 1) {
         x = d3.scaleLinear().domain([minx, maxx]).range([0, width]);
@@ -38,7 +41,6 @@ function drawBubble(id, prop, data, c1, c2, c3, c4, mode) {
     } else {
         x = d3
             .scalePoint()
-            //.scaleBand()
             .rangeRound([0, width])
             .padding(0.1)
             .domain(rawData.map(function (d) {
@@ -47,7 +49,6 @@ function drawBubble(id, prop, data, c1, c2, c3, c4, mode) {
 
         y = d3
             .scalePoint()
-            // .scaleBand()
             .rangeRound([0, height])
             .padding(0.1)
             .domain(rawData.map(function (d) {
@@ -76,4 +77,5 @@ function drawBubble(id, prop, data, c1, c2, c3, c4, mode) {
     appendYaxis(svg, y, x);
     appendYlabel(svg, c2, width, height, x);
     // appendLegend(data, svg, domain, 20, -40, color);
+    return [height, width];
 }
